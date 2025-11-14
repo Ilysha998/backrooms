2e3b4af9da5965fb8ab5f2264ae61707ff0186f4
Ilysha998 <sukhachew.ilya@gmail.com>
2025-11-14 15:28:37 +0700

    deadcode
    СУКА НЕ ОТ ЗАКОМЕНТИЛ

--- scripts/crossroad.gd
+++
@@ -1,7 +0,0 @@
-class_name Crossroad
-extends Node3D
-
-@export var has_north_door: bool = true
-@export var has_south_door: bool = true
-@export var has_east_door: bool = true
-@export var has_west_door: bool = true
