4122c128cd93e074fdcef3734668d9d9c6d0c4b5
Ilysha998 <sukhachew.ilya@gmail.com>
2025-11-15 12:34:30 +0700

    ЗШЯВУС 2312123213

--- addons/fpc/die_scene.gd
+++ addons/fpc/die_scene.gd
@@ -1,15 +1,13 @@
 extends Control
 
-# Эта функция вызывается при нажатии кнопки перезапуска.
-func _on_restart_button_pressed():
-	# Сначала снимаем игру с паузы, чтобы перезагрузка сработала корректно.
-	get_tree().paused = false
-	# Перезагружаем текущую игровую сцену.
-	get_tree().reload_current_scene()
-
 # Эта функция вызывается при нажатии кнопки меню.
 func _on_menu_button_pressed():
 	# Снимаем игру с паузы перед переходом в другую сцену.
 	get_tree().paused = false
 	# Переходим к сцене главного меню.
 	get_tree().change_scene_to_file("res://scenes/mmscene.tscn")
+
+
+func _on_continue_button_pressed() -> void:
+	get_tree().paused = false
+	hide()
