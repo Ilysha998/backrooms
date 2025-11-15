4e8d9dac4399d648ce36c50ebd40ce9104e9de2d
LayRad <ANUMATIK@mail.ru>
2025-11-15 08:19:59 +0700

    Баг фиксы - свет + пауза во время загрузки

--- scripts/loading.gd
+++ scripts/loading.gd
@@ -1,6 +1,5 @@
 extends Node
 
-
 @onready var wall_spawner = $MapGenerator
 @onready var lamp_spawner = $LampGenerator
 @onready var item_spawner = $ItemsSpawner
@@ -12,17 +11,19 @@ func _ready():
 
 # Ключевое слово 'async' позволяет использовать 'await' внутри функции
 func start_loading_process():
+	# --- ШАГ 1: ВКЛЮЧАЕМ ПАУЗУ ---
+	# Замораживаем весь игровой мир. 
+	# Экран загрузки продолжит работать, так как его Process Mode установлен в 'Always'.
+	get_tree().paused = true
+	
 	# 0. Показываем экран загрузки
 	loading_screen.show()
 	
 	# 1. Генерация стен
 	loading_screen.update_status("Генерация уровня...")
-	# Подключаемся к сигналу прогресса
 	wall_spawner.progress_updated.connect(loading_screen.update_progress)
-	wall_spawner.generate_level() # Запускаем генерацию
-	# Ждем, пока спавнер не отправит сигнал о завершении
+	wall_spawner.generate_level()
 	await wall_spawner.generation_finished 
-	# Отключаемся, чтобы сигнал не влиял на другие этапы
 	wall_spawner.progress_updated.disconnect(loading_screen.update_progress)
 
 	# 2. Генерация ламп
@@ -36,17 +37,20 @@ func start_loading_process():
 	loading_screen.update_status("Подготовка...")
 	item_spawner.start_spawning()
 	
-	# Небольшая задержка для красоты
 	await get_tree().create_timer(0.5).timeout 
 
 	# 4. Завершение
-	loading_screen.update_progress(1.0) # Устанавливаем 100%
+	loading_screen.update_progress(1.0)
 	loading_screen.update_status("Готово!")
 	
-	await get_tree().create_timer(1.0).timeout # Показываем "Готово!" на секунду
+	await get_tree().create_timer(1.0).timeout
 	
 	# Прячем экран загрузки
 	loading_screen.hide()
+	
+	# --- ШАГ 2: ВЫКЛЮЧАЕМ ПАУЗУ ---
+	# "Размораживаем" игру. Теперь лампы начнут мигать, физика заработает.
+	get_tree().paused = false
 	
 	# Здесь можно включить управление игроком, запустить музыку и т.д.
 	print("ЗАГРУЗКА ЗАВЕРШЕНА!")
